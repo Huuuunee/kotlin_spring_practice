356c3ef34813fbc3922f5266ec443517d2798ef6
Huuuunee <s21024@gsm.hs.kr>
2022-10-31 23:30:03 +0900

    :recycle: 유저가 BaseIdEntity를 상속받도록 변환

---
+++ src/main/kotlin/com/example/helloworld/global/entity/BaseIdEntity.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld.global.entity
+
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+open class BaseIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+}
