2bf22d245ed058dd8380a70e802ed5782ff27ce6
Huuuunee <s21024@gsm.hs.kr>
2022-10-31 23:30:39 +0900

    :heavy_plus_sign: 스프링 스타트 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
-	implementation("org.springframework.boot:spring-boot-starter-mail:2.6.7")
+	implementation("org.springframework.boot:spring-boot-starter-mail:2.7.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
