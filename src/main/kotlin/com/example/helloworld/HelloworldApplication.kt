8a426972fe12652da8f65161b853a540200b054d
Huuuunee <s21024@gsm.hs.kr>
2022-11-03 23:29:14 +0900

    :heavy_plus_sign: 의존성 추가 및 수정

--- src/main/kotlin/com/example/helloworld/HelloworldApplication.kt
+++ src/main/kotlin/com/example/helloworld/HelloworldApplication.kt
@@ -1,9 +1,11 @@
 package com.example.helloworld
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class HelloworldApplication
 
 fun main(args: Array<String>) {
