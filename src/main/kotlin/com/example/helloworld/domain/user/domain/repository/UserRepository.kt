b1ebae6f141263138e7285f10aa46dec5fc3e4d2
Huuuunee <s21024@gsm.hs.kr>
2022-11-09 14:07:34 +0900

    :recycle: Null 가능 하도록 타입 변환

--- src/main/kotlin/com/example/helloworld/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/helloworld/domain/user/domain/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.example.helloworld.domain.user.domain.repository
 
 import com.example.helloworld.domain.user.domain.entitiy.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface UserRepository:JpaRepository<User,Long>{
-    fun findByEmail(email:String): User
+interface UserRepository: JpaRepository<User,Long>{
+    fun findByEmail(email:String): User?
+    fun existsByEmail(email:String): Boolean
 }
