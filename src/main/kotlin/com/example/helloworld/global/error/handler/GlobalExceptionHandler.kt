4ec6dfbe34b08e470ef586b5217e42819ac0569d
Huuuunee <s21024@gsm.hs.kr>
2022-12-06 17:01:29 +0900

    :loud_sound: 에러발생시 로그 출력하도록 변경

--- src/main/kotlin/com/example/helloworld/global/error/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/helloworld/global/error/handler/GlobalExceptionHandler.kt
@@ -1,18 +1,29 @@
 package com.example.helloworld.global.error.handler
 
+import com.example.helloworld.global.error.ErrorCode
 import com.example.helloworld.global.error.ErrorResponse
 import com.example.helloworld.global.error.exception.BasicException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.net.BindException
+import javax.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(
+) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
+
     @ExceptionHandler(BasicException::class)
-    fun handleBaseException(ex: BasicException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(ex.errorCode);
-        return ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.valueOf(errorResponse.code))
+    fun globalExceptionHandler(request: HttpServletRequest, e: BasicException): ResponseEntity<ErrorResponse?> {
+        log.error(request.requestURL.toString())
+        log.error(e.errorCode.msg)
+        val errorCode: ErrorCode = e.errorCode
+        return ResponseEntity(
+            ErrorResponse(code = errorCode.code, msg = errorCode.msg),
+            HttpStatus.valueOf(errorCode.code)
+        )
     }
 }
