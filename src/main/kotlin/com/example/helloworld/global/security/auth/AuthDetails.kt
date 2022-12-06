4d9442b694f187364df1f31d0b7b796f33a6a177
Huuuunee <s21024@gsm.hs.kr>
2022-12-06 16:58:04 +0900

    :recycle: authDetails 수정

--- src/main/kotlin/com/example/helloworld/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/com/example/helloworld/global/security/auth/AuthDetails.kt
@@ -5,26 +5,19 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class AuthDetails(
-        private val user: User
-): UserDetails {
-    override fun getAuthorities(): Collection<GrantedAuthority?> =
-            listOf()
+    private val user: User
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority>? = null
 
-    override fun getPassword(): String? =
-            null
+    override fun getPassword(): String? = null
 
-    override fun getUsername(): String =
-            user.email
+    override fun getUsername(): String = user.email
 
-    override fun isAccountNonExpired(): Boolean =
-            true
+    override fun isAccountNonExpired(): Boolean = true
 
-    override fun isAccountNonLocked(): Boolean =
-            true
+    override fun isAccountNonLocked(): Boolean = true
 
-    override fun isCredentialsNonExpired(): Boolean =
-            true
+    override fun isCredentialsNonExpired(): Boolean = true
 
-    override fun isEnabled(): Boolean =
-            isAccountNonExpired && isAccountNonLocked && isCredentialsNonExpired
+    override fun isEnabled(): Boolean = true
 }
