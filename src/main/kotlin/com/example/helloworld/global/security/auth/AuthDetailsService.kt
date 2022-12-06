6737096f58ad84792b5eace2f4b31971f4a739e3
Huuuunee <s21024@gsm.hs.kr>
2022-12-06 16:59:03 +0900

    :recycle: 일관성 있게 변수명 변경

--- src/main/kotlin/com/example/helloworld/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/example/helloworld/global/security/auth/AuthDetailsService.kt
@@ -10,10 +10,11 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true)
 class AuthDetailsService(
-        private val userRepository: UserRepository
+    private val userRepository: UserRepository
 ): UserDetailsService {
-    override fun loadUserByUsername(email: String): UserDetails {
-        val user = userRepository.findByEmail(email) ?: throw UserNotFoundException()
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByEmail(username) ?: throw UserNotFoundException()
         return AuthDetails(user)
     }
+
 }
