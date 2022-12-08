1580094eeb6cc01fa3f12a9245c1bdea10a5662d
Huuuunee <s21024@gsm.hs.kr>
2022-12-08 20:07:03 +0900

    :recycle: util 을 interface와 구현체로 쪼갰습니다

--- src/main/kotlin/com/example/helloworld/domain/user/util/UserUtil.kt
+++ src/main/kotlin/com/example/helloworld/domain/user/util/UserUtil.kt
@@ -1,27 +1,9 @@
 package com.example.helloworld.domain.user.util
 
 import com.example.helloworld.domain.user.domain.entitiy.User
-import com.example.helloworld.domain.user.domain.repository.UserRepository
-import com.example.helloworld.domain.user.exception.UserNotFoundException
-import com.example.helloworld.global.security.auth.AuthDetails
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.stereotype.Component
 
-@Component
-class UserUtil(
-    private val userRepository: UserRepository
-) {
-    fun fetchCurrentUser(): User {
-        val principal = SecurityContextHolder.getContext().authentication.principal
-        val email = if (principal is UserDetails) {
-            (principal as AuthDetails).username
-        } else {
-            principal.toString()
-        }
-        return fetchUserByEmail(email)
-    }
+interface UserUtil {
+    fun fetchCurrentUser(): User
 
-    fun fetchUserByEmail(email: String): User =
-        userRepository.findByEmail(email) ?: throw UserNotFoundException()
+    fun fetchUserByEmail(email: String): User
 }
