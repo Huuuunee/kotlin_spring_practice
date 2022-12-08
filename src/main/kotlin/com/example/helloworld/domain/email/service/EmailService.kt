0ef283272c72a271fd746425fbde35f0eb986b34
Huuuunee <s21024@gsm.hs.kr>
2022-12-08 15:11:29 +0900

    :fire: 통합되어있던 service와 구현체 삭제

--- src/main/kotlin/com/example/helloworld/domain/email/service/EmailService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.helloworld.domain.email.service
-
-import com.example.helloworld.domain.email.presentation.dto.request.EmailSentRequestDto
-
-interface EmailService{
-    fun emailSend(emailSentRequestDto: EmailSentRequestDto)
-    fun emailVerified(email: String, uuid: String)
-}  
