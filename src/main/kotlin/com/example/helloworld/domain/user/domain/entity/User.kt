f945d9114fbd7b8701185944ed3a76284a4a5409
Huuuunee <s21024@gsm.hs.kr>
2022-12-10 23:36:07 +0900

    :sparkles: 게시글 엔티티 연결

---
+++ src/main/kotlin/com/example/helloworld/domain/user/domain/entity/User.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld.domain.user.domain.entity
+
+import com.example.helloworld.domain.post.domain.entity.Post
+import com.example.helloworld.global.entity.BaseIdEntity
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Entity
+@Table
+class User(
+    val email: String,
+
+    val password: String,
+
+    val name: String,
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    val post: List<Post>?
+) : BaseIdEntity() {
+}
