3222298755b8812ad9335da3f4f57c8fd3306c39
Huuuunee <s21024@gsm.hs.kr>
2022-12-10 23:35:41 +0900

    :recycle: 오타수정

--- src/main/kotlin/com/example/helloworld/domain/auth/service/impl/LogOutServiceImpl.kt
+++ src/main/kotlin/com/example/helloworld/domain/auth/service/impl/LogOutServiceImpl.kt
@@ -2,7 +2,7 @@ package com.example.helloworld.domain.auth.service.impl
 
 import com.example.helloworld.domain.auth.domain.repository.RefreshTokenRepository
 import com.example.helloworld.domain.auth.service.LogOutService
-import com.example.helloworld.domain.user.domain.entitiy.User
+import com.example.helloworld.domain.user.domain.entity.User
 import com.example.helloworld.domain.user.util.UserUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
