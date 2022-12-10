7d075c86b64bba23f45569e61196487f9ad90946
Huuuunee <s21024@gsm.hs.kr>
2022-12-10 23:36:31 +0900

    :sparkles: 게시글 엔티티 생

---
+++ src/main/kotlin/com/example/helloworld/domain/post/domain/entity/Post.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld.domain.post.domain.entity
+
+import com.example.helloworld.domain.user.domain.entity.User
+import com.example.helloworld.global.entity.BaseIdEntity
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table
+class Post(
+    @Column(nullable = false, length = 10000)
+    val content: String,
+    @Column(nullable = false, length = 100)
+    val title: String,
+    val views: Long,
+    val localDateTime: LocalDateTime,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    val user: User
+) : BaseIdEntity(
+)
