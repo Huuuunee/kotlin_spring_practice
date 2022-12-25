233dec14640a1381414112eda2b74d0bbbd35775
Huuuunee <s21024@gsm.hs.kr>
2022-12-25 22:15:08 +0900

    :sparkles: 특정 게시글 불러오기 + 조회수 증가

--- src/main/kotlin/com/example/helloworld/domain/post/service/GetPostInfoService.kt
+++ src/main/kotlin/com/example/helloworld/domain/post/service/GetPostInfoService.kt
@@ -1,9 +1,12 @@
 package com.example.helloworld.domain.post.service
 
+import com.example.helloworld.domain.post.presentation.data.dto.GetPostInfoDto
 import com.example.helloworld.domain.post.presentation.data.response.PostingListResponseDto
+import com.example.helloworld.domain.post.presentation.data.response.PostingResponseDto
 import org.springframework.data.domain.Pageable
 
 interface GetPostInfoService {
     fun execute(pageable: Pageable): PostingListResponseDto
+    fun execute(getPostInfoDto: GetPostInfoDto): PostingResponseDto
     fun execute(): PostingListResponseDto
 }
