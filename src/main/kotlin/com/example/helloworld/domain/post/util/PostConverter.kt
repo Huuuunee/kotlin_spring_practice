c4beb80efceba17d76edbef6c2ea54dff7ddd8e7
Huuuunee <s21024@gsm.hs.kr>
2022-12-25 22:17:05 +0900

    :sparkles: 불러온 postId Dto추가

--- src/main/kotlin/com/example/helloworld/domain/post/util/PostConverter.kt
+++ src/main/kotlin/com/example/helloworld/domain/post/util/PostConverter.kt
@@ -2,10 +2,13 @@ package com.example.helloworld.domain.post.util
 
 import com.example.helloworld.domain.post.domain.entity.Post
 import com.example.helloworld.domain.post.presentation.data.dto.CreatePostDto
+import com.example.helloworld.domain.post.presentation.data.dto.GetPostInfoDto
 import com.example.helloworld.domain.post.presentation.data.request.CreatePostRequestDto
+import com.example.helloworld.domain.post.presentation.data.request.GetPostInfoRequestDto
 import com.example.helloworld.domain.user.domain.entity.User
 
 interface PostConverter {
     fun toDto(request: CreatePostRequestDto): CreatePostDto
+    fun toDto(request: Long): GetPostInfoDto
     fun toEntity(createPostDto: CreatePostDto, userInfo: User): Post
 }
--- src/main/kotlin/com/example/helloworld/domain/post/util/impl/PostConverterImpl.kt
+++ src/main/kotlin/com/example/helloworld/domain/post/util/impl/PostConverterImpl.kt
@@ -2,7 +2,9 @@ package com.example.helloworld.domain.post.util.impl
 
 import com.example.helloworld.domain.post.domain.entity.Post
 import com.example.helloworld.domain.post.presentation.data.dto.CreatePostDto
+import com.example.helloworld.domain.post.presentation.data.dto.GetPostInfoDto
 import com.example.helloworld.domain.post.presentation.data.request.CreatePostRequestDto
+import com.example.helloworld.domain.post.presentation.data.request.GetPostInfoRequestDto
 import com.example.helloworld.domain.post.util.PostConverter
 import com.example.helloworld.domain.user.domain.entity.User
 import org.springframework.stereotype.Component
@@ -11,7 +13,15 @@ import java.time.LocalDateTime
 @Component
 class PostConverterImpl() : PostConverter {
     override fun toDto(request: CreatePostRequestDto): CreatePostDto =
-        CreatePostDto(request.title, request.content)
+        CreatePostDto(
+            title = request.title,
+            content = request.content
+        )
+
+    override fun toDto(request: Long): GetPostInfoDto =
+        GetPostInfoDto(
+            postId = request
+        )
 
     override fun toEntity(createPostDto: CreatePostDto, userInfo: User): Post =
         Post(
@@ -21,4 +31,5 @@ class PostConverterImpl() : PostConverter {
             createDateTime = LocalDateTime.now(),
             user = userInfo
         )
+
 }
